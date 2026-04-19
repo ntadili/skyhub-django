c4f06b6b42f2dad2f93cc55b375c5bee3bda507a
ntadili <businessnasser6@gmail.com>
2026-04-19 15:43:02 +0100

    Create templates structure, implement initial dashboard UI and titles for rest of sections and add sample data in the admin panel

--- main/admin.py
+++ main/admin.py
@@ -2,8 +2,10 @@ from django.contrib import admin
 from .models import Profile
 from .models import Department
 from .models import Team
+from .models import Meeting
 # Register your models here.
 
 admin.site.register(Profile)
 admin.site.register(Department)
 admin.site.register(Team)
+admin.site.register(Meeting)
--- skyhub/settings.py
+++ skyhub/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'skyhub.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'main' / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
